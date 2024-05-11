ea28a7f4c4ec7796bd80598cc3831bf5b49481b8
Zohre Bodaghi <zohreh_bodaghi73@yahoo.com>
2024-05-11 23:50:09 +0330

    filter product by category and change the style of each category button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 const searchInput = document.getElementById("search-input");
 const productItem = document.querySelectorAll(".product-item");
+const buttons = document.querySelectorAll(".filter");
+
+const changeClass = (filter) => {
+    console.log(filter);
+    buttons.forEach((button) => {
+        if (button.dataset.filter === filter) {
+            button?.classList?.add("selected")
+        } else {
+            button?.classList?.remove("selected")
+        }
+    })
+}
 
 const searchHandler = (event) => {
     const searchValue = event.target.value.toLowerCase().trim();
@@ -13,4 +25,26 @@ const searchHandler = (event) => {
     })
 }
 
+const filterHandler = (event) => {
+    const filter = event.target.dataset.filter;
+    changeClass(filter);
+
+    productItem.forEach((product) => {
+        const productCategory = product.dataset.category;
+        if (filter === "all") {
+            product.style.display = "block";
+        } else {
+            if (productCategory === filter) {
+                product.style.display = "block";
+            } else {
+                product.style.display = "none";
+            }
+        }
+    })
+}
+
 searchInput.addEventListener("keyup", searchHandler);
+
+buttons.forEach((button) => {
+    button.addEventListener("click", filterHandler)
+})
